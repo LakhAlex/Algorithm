b95ff7ce30bd13c1f2b0d82e2b85ae84c405ac2c
Lakh <135232784+LakhAlex@users.noreply.github.com>
2023-10-09 21:13:00 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int [photo.length];
+        HashMap<String, Integer> a = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < name.length; i++){
+            a.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++) {
+        	for(int j = 0; j <photo[i].length; j++) {
+        		if(a.get(photo[i][j]) == null) continue;
+        		answer[i] += a.get(photo[i][j]);
+        	}
+        }
+        
+        return answer;
+    }
+}
